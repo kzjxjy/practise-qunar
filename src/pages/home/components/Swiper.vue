375215f44dd76a5604983eae0f321238d9bd058c
blanolic <carolinechau1995@gmail.com>
2022-11-14 21:56:03 +0800

    添加轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/2104/c6/c6d76981ae21f58ea3.water.jpg_600x330_581fd255.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/2005/4a/4ae86ceefd441536a3.water.jpg_600x330_745eff14.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
