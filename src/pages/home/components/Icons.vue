7fbff89ede11e1a7f413f5f065f29096dffa8f5f
blanolic <carolinechau1995@gmail.com>
2022-11-16 20:31:31 +0800

    热销栏更新

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -76,34 +76,36 @@ export default {
     width: 100%
     height: 0
     padding-bottom: 50%
-.icon
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img
-        position: absolute
-        top: 0
-        right: 0
-        left: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-            display: block
-            margin: 0 auto
-            height: 100%
-            width: 100%
-    .icon-desc
-        position: absolute
-        bottom: 0
-        right: 0
-        left: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTextColor
-        ellipsis()
+.icons
+  margin-top: .2rem
+  .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+          position: absolute
+          top: 0
+          right: 0
+          left: 0
+          bottom: .44rem
+          box-sizing: border-box
+          padding: .1rem
+          .icon-img-content
+              display: block
+              margin: 0 auto
+              height: 100%
+              width: 100%
+      .icon-desc
+          position: absolute
+          bottom: 0
+          right: 0
+          left: 0
+          height: .44rem
+          line-height: .44rem
+          text-align: center
+          color: $darkTextColor
+          ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img :src="item.imgUrl" class="item-img">
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <button class="item-button">查看详情</button>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/9e/9e643f96c84b5c2ea3.water.jpg_200x200_2a3eef5b.jpg',
+        title: '上海欢乐谷',
+        desc: '上海市松江区佘山镇林湖路888号'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/9e/9e643f96c84b5c2ea3.water.jpg_200x200_2a3eef5b.jpg',
+        title: '上海欢乐谷',
+        desc: '上海市松江区佘山镇林湖路888号'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/9e/9e643f96c84b5c2ea3.water.jpg_200x200_2a3eef5b.jpg',
+        title: '上海欢乐谷',
+        desc: '上海市松江区佘山镇林湖路888号'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+.title
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+  margin-top: .2rem
+.item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+        width: 1.7rem
+        height: 1.7rem
+        padding: .1rem
+    .item-info
+        flex: 1 //自动撑开屏幕宽度
+        padding: .1rem
+        min-width: 0
+        .item-title
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            padding-top: .06rem
+            line-height: .4rem
+            color: #ccc
+            ellipsis()
+        .item-button
+            line-height: .5rem
+            margin-top: .2rem
+            background: #ff9300
+            padding: 0 .2rem
+            border-radius: .06rem
+            color: #fff
+</style>
