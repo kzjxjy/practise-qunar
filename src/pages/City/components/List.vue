ddfebdeedb97ba959cc44eb44867cd2fdfde604a
blanolic <carolinechau1995@gmail.com>
2022-11-21 21:42:33 +0800

    城市页面基本布局更新

---
+++ src/pages/City/components/List.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-topbottom">当前城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">
+                        上海
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">热门城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">
+                        上海
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        上海
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        上海
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        上海
+                    </div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">
+                        上海
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.border-topbottom
+    &:before
+        border-color: #ccc
+    &:after
+        border-color: #ccc
+.border-bottom
+    &:before
+        border-color: #ccc
+.list
+    position: absolute
+    overflow: hidden
+    top:1.58rem
+    left: 0
+    bottom: 0
+    right: 0
+    background: blue
+    .title
+        line-height: .44rem
+        background: #eee
+        padding-left: .2rem
+        color: #666
+        font-size: .26rem
+    .button-list
+        padding: .1rem .6rem .1rem .1rem
+        overflow: hidden
+        .button-wrapper
+            width: 33.33%
+            float: left
+            .button
+                padding: .1rem 0
+                text-align: center
+                margin: .1rem
+                border: .02rem solid #ccc
+                border-radius: .06rem
+    .item-list
+        .item
+            line-height: .76rem
+            color: #666
+            padding-left: .2rem
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,11 +6,13 @@
         <div class="header-input">
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
-            </div>
-        <div class="header-right">
-            {{this.city}}
-            <span class="iconfont arrow-icon">&#xe600;</span>
         </div>
+        <router-link to='/city' >
+            <div class="header-right">
+                {{this.city}}
+                <span class="iconfont arrow-icon">&#xe600;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
 @import '~styles/varibles.styl'
 .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -51,6 +53,7 @@ export default {
         width 1.24rem
         float: right
         text-align: center
+        color: white
         .arrow-icon
             margin-left: -.04rem
             font-size: .2rem
